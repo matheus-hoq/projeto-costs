09e48b1b915158083b04f93fe4c37e94edf751cc
matheus_hoq <henriquematheus4000@gmail.com>
2021-12-13 11:07:05 -0300

    Utilizando placeholder

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ function App() {
       return a + b
     }
 
+    const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
       <h2>Alterando o JSX</h2>
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }
