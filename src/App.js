4017918f6795af34f8cb94039fb0bdabc663c258
matheus_hoq <henriquematheus4000@gmail.com>
2021-12-13 11:02:42 -0300

    Inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Matheus"
+
+  const newName = name.toUpperCase()
+
+    function sum(a, b){
+      return a + b
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1,2)}</p>
     </div>
   );
 }
