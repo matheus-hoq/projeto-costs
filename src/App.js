154243ccc79f6d23c622b709790a26d08a0f2b1d
matheus_hoq <henriquematheus4000@gmail.com>
2021-12-15 09:56:24 -0300

    Fragments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
         profissao="Programador" 
         foto="https://via.placeholder.com/150"
       />
+      <List />
     </div>
   )
 }
